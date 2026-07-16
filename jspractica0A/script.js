596a048a1c36f8814552ed22055474578698e08c
dar1sitaa <mxchxgarcia09@gmail.com>
2026-07-16 10:50:42 -0600

    nose
    ..

---
+++ jspractica0A/script.js
@@ -0,0 +1,19 @@
+
+
+function Revisar(){
+
+let caja = document.getElementById("mensaje");
+//Obtenemos el parrafo donde ira el mensaje
+
+caja.innerHTML="Respuesta correcta! so nais mai fren"
+
+}
+caja.classlist.remove("incorrecto")
+function Revisarmal(){
+
+let caja = document.getElementById("mensaje");
+//Obtenemos el parrafo donde ira el mensaje
+
+caja.innerHTML="Respuesta mal! sorry hermano"
+
+}
